f5171374b7c7b3b76631b2f28617a72a112390da
MyMac <mymac@MacBook-Pro.local>
2020-06-21 10:10:31 -0400

    rubo

--- app-name/app/controllers/application_controller.rb
+++ app-name/app/controllers/application_controller.rb
@@ -1,47 +1,21 @@
 require './config/environment'
 class ApplicationController < Sinatra::Base
-
-
-  configure do
+  configure do 
       set :views, 'app/views'
-      set :public_folder, 'public'
       enable :sessions
-      set :session_secret, SESSION_SECRET
+      set :session_secret, ""
   end
-
-  get '/' do
+  get '/' do 
       erb :index
-  end
+  end 
+  get '/failure' do
+      erb :failure 
+  end 
+  get '/login' do 
+      erb :login 
+  end 
+  get '/signup' do 
+      erb :signup 
+  end 
 
-  not_found do
-    @messages = "this page does not exist"
-    erb :'error'
-  end
-
-  helpers do
-
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      Trainer.find_by_id(session[:user_id])
-    end
-
-    def authenticate
-      if !logged_in?
-        redirect '/login'
-      end
-    end
-
-    def clean(params)
-      new_params = params.dup
-      params.each do |k, v|
-        new_params[k] = Rack::Utils.escape_html(v)
-      end
-      return new_params
-    end
-
-  end
-
-end
+end  
