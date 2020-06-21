769fbff0ab547716549b5a94c980738c8dba6da7
MyMac <mymac@MacBook-Pro.local>
2020-06-20 20:20:45 -0400

    testing

--- app-name/app/controllers/application_controller.rb
+++ app-name/app/controllers/application_controller.rb
@@ -1,14 +1,47 @@
 require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
+
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+      set :views, 'app/views'
+      set :public_folder, 'public'
+      enable :sessions
+      set :session_secret, SESSION_SECRET
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+      erb :index
+  end
+
+  not_found do
+    @messages = "this page does not exist"
+    erb :'error'
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      Trainer.find_by_id(session[:user_id])
+    end
+
+    def authenticate
+      if !logged_in?
+        redirect '/login'
+      end
+    end
+
+    def clean(params)
+      new_params = params.dup
+      params.each do |k, v|
+        new_params[k] = Rack::Utils.escape_html(v)
+      end
+      return new_params
+    end
+
   end
 
 end
--- app-name/config/environment.rb
+++ app-name/config/environment.rb
@@ -1,4 +1,5 @@
 ENV['SINATRA_ENV'] ||= "development"
+require_relative './config/env'
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
