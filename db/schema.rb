3241f9fcfa86eb7a2f279edfb4d39b1427642345
MyMac <mymac@MacBook-Pro.local>
2020-07-08 05:54:14 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
 ActiveRecord::Schema.define(version: 2019_08_15_013934) do
 
-    create_table "digimons", force: :cascade do |t|
-      t.string "name"
-      t.string "nickname"
-      t.string "type"
-      t.integer "trainer_id"
+    create_table "books", force: :cascade do |t|
+      t.string "publisher"
+      t.string "year"
+      t.string "title"
+      t.string "series"
+      t.integer "user_id"
     end
   
     create_table "users", force: :cascade do |t|
       t.string "email"
-      t.string "trainer_name"
+      t.string "username"
       t.string "password_digest"
     end
   
