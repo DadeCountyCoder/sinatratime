53843f56b63441b4431b3c85420615ea3e4dba17
DadeCountyCoder <dadecountycoder@gmail.com>
2020-10-30 18:18:51 -0400

    config/env

--- config.ru
+++ config.ru
@@ -5,6 +5,6 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
 end
 
 use Rack::MethodOverride
-run ApplicationController
 use UserController
 use BookController
+run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,13 @@
-require 'bundler/setup'
-Bundler.require
 #require 'sinatra/flash'
+ENV['SINATRA_ENV'] ||= "development"
 
-require_relative '../app/models/user'
-require_relative '../app/controllers/application_controller'
-require_relative '../app/controllers/book_controller'
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require './app/controllers/application_controller'
 require_all 'app'
