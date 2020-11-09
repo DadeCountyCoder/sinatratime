663da9e06cca33579c3d50bc44a33b059b730162
DadeCountyCoder <dadecountycoder@gmail.com>
2020-11-09 11:08:35 -0500

    working on validation

--- app/models/books.rb
+++ app/models/books.rb
@@ -1,3 +1,12 @@
 class Books < ActiveRecord::Base
+    validates :year, :title, :series, :user_id, presence: true
+   
     belongs_to :user
 end
+
+
+
+    #   t.string "year"
+    #   t.string "title"
+    #   t.string "series"
+    #   t.integer "user_id"
